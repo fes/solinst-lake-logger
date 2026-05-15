328ff4ba74c3a757b7fc7a1fab8c7cb74aa5a375
fes <fes@feslabs.com>
2026-05-14 19:40:04 -0700

    Add tracked secrets template for local compile-time config

---
+++ secrets_example.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Copy this file to secrets_local.h, fill in your real values locally,
+// and keep secrets_local.h out of git.
+
+#define WIFI_SSID_VALUE "YOUR_WIFI_SSID"
+#define WIFI_PASS_VALUE "YOUR_WIFI_PASSWORD"
+#define DEVICE_ID_VALUE "opta-well-01"
+#define SHARED_SECRET_VALUE "PUT_A_LONG_RANDOM_SECRET_HERE"
+#define DEPLOYMENT_ID_VALUE "PUT_YOUR_DEPLOYMENT_ID_HERE"
+#define DISPLAY_ON_SECONDS_VALUE 15UL
